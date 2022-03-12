5541b88a7d7a530ab9dd9d13c157c4104cb8c75f
hugosenari <hugosenari@gmail.com>
2022-03-12 02:18:40 -0300

    chore: re index openal

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = ''An OpenAL wrapper.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."openal-master".url = "path:./master";
+  inputs."openal-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."openal-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."openal-0_1_1".url = "path:./0_1_1";
+  inputs."openal-0_1_1".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."openal-0_1_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."openal-v0_1_0".url = "path:./v0_1_0";
+  inputs."openal-v0_1_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."openal-v0_1_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''An OpenAL wrapper.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-openal-master.flake = false;
+  inputs.src-openal-master.owner = "treeform";
+  inputs.src-openal-master.ref   = "refs/heads/master";
+  inputs.src-openal-master.repo  = "openal";
+  inputs.src-openal-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-openal-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-openal-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
